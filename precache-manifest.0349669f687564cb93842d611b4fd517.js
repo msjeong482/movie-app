c87c63eb2a5ffb1bb752e0276450ded27fc45229
Minseong Jeong <minseong@Minseongs-MacBook-Pro-2.local>
2020-07-13 21:16:57 +0900

    Updates

---
+++ precache-manifest.0349669f687564cb93842d611b4fd517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2de5c7ad0e4591786fe8c3249a4a7b",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "5d9c6657f08a2ffaffec",
+    "url": "/movie-app/static/css/main.ac6a4719.chunk.css"
+  },
+  {
+    "revision": "9dce85a7ddc84d2282ca",
+    "url": "/movie-app/static/js/2.7bec502f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.7bec502f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9c6657f08a2ffaffec",
+    "url": "/movie-app/static/js/main.93627e44.chunk.js"
+  },
+  {
+    "revision": "f1d1464ce1226f453fe5",
+    "url": "/movie-app/static/js/runtime-main.4344d1ad.js"
+  }
+]);
